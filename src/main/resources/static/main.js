2624f23e17793e469c9a92d473ac690ce7bd2f49
otcz <carrillozuletaoscar@gmail.com>
2023-01-29 17:26:14 -0500

    f

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -6,11 +6,13 @@ function enviarDatosUsuario(){
 
  var precio=document.getElementById("precio");
  var nombre=document.getElementById("nombre");
+  var img=document.getElementById("img");
    var btn_finalizar_compra=document.getElementById("btn_finalizar_compra");
 
     // btn_finalizar_compra.href = "https://mpago.li/2pVFLGS";
      alert(precio.text);
      alert(nombre.text);
+     alert(img.text);
 
 
 

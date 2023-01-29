2d41708397caea84b852beb7ec537b6d924a58c9
otcz <carrillozuletaoscar@gmail.com>
2023-01-29 17:30:53 -0500

    f

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -14,6 +14,10 @@ function enviarDatosUsuario(){
      alert(nombre.text);
      alert(img.text);
 
+      var win = window.open(url, '_blank');
+             // Cambiar el foco al nuevo tab (punto opcional)
+             win.focus();
+
 
 
    }

c0b9133bfadfcf047b9c009eb3a1a43806157a17
otcz <carrillozuletaoscar@gmail.com>
2023-01-29 15:10:32 -0500

    f

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -3,18 +3,15 @@
 
 
 function enviarDatosUsuario(){
-var nombreabuelo = "Juan"
-var nombreabuela = "María"
- var btn_pagar=document.getElementById("btn_pagar");
-        var nombres=document.getElementById("nombres");
-        var telefono=document.getElementById("telefono");
-        var pais=document.getElementById("pais");
-        var email=document.getElementById("email");
-        var mensaje=document.getElementById("mensaje");
 
-     var btn_enviar=document.getElementById("btn_enviar");
-     btn_enviar.text="Gracias!"
-     var info="*Nombre aspirante:* "+nombres.value+" *Teléfono:* "+telefono.value+" *País:* "+pais.value+" *E-Mail:* "+email.value+" *Mensaje:* "+mensaje.value+""
+ var precio=document.getElementById("precio");
+ var nombre=document.getElementById("nombre");
+   var btn_finalizar_compra=document.getElementById("btn_finalizar_compra");
 
-     btn_enviar.href = "https://wa.me/+16098752052?text="+info+"";
+    // btn_finalizar_compra.href = "https://mpago.li/2pVFLGS";
+     alert(precio.text);
+     alert(nombre.text);
+
+
+
    }

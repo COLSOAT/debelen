0e3a41b6edfe556214c8a0507abf14779f7782fc
otcz <carrillozuletaoscar@gmail.com>
2023-01-30 10:08:38 -0500

    f

---
+++ src/main/resources/static/solicitudcretido/solicitar.js
@@ -0,0 +1,10 @@
+
+ alert("oscar");
+ var valor=document.getElementById("valor");
+ var btnsolicitar=document.getElementById("btnsolicitar");
+
+
+ function solicitacredito(){
+console.log(valor);
+
+}
